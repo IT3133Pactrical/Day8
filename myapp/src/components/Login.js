6268566085b6e9b5c638f075387800ec44f512d3
chamithjeewantha <139670985+chamithjeewantha@users.noreply.github.com>
2024-11-26 09:08:35 +0000

    import navigate and add dash for navigate

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,4 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Login(){
+    const navigate=useNavigate();
+    const aunthenticate=()=>{
+        navigate('/dash')
+    }
     return(
         <div>
             <h1>Login here</h1>
