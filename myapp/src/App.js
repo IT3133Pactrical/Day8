5e6bb80dbdb60613ae302a6d5aa4a0a1e22acf34
chamithjeewantha <139670985+chamithjeewantha@users.noreply.github.com>
2024-11-26 08:46:02 +0000

    create link between pages

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,6 +5,15 @@ import { BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-d
 function App() {
   return (
     <div className="App">
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contactus">ContactUs</Link></li>
+          </ul>
+        </nav>
+      </Router>
      <Router>
 
       <Routes>
